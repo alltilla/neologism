355bc1169a34e90586de0253b798e696f8f90711
Attila Szakacs <szakacs.attila96@gmail.com>
2021-10-10 14:15:03 +0200

    version: bump to 0.0.3
    Signed-off-by: Attila Szakacs <szakacs.attila96@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, str(Path(__file__).parents[2].resolve()))
 project = "neologism"
 copyright = "2021, Attila Szakacs"
 author = "Attila Szakacs"
-release = "0.0.2"
+release = "0.0.3"
 extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
